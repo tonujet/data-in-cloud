39cd11258e6b3b058db052e66ceb479d5881bff3
Antonio <98595473+tonujet@users.noreply.github.com>
2024-02-29 15:49:19 +0200

    Add service layer for repo entity

---
+++ lab1/src/web/utils/mod.rs
@@ -0,0 +1,12 @@
+use axum::body::Body;
+use axum::extract::{Host, Request};
+use axum::response::IntoResponse;
+
+use crate::web::error::ApiError;
+
+pub async fn api_not_found_handler(Host(hostname): Host, req: Request<Body>) -> impl IntoResponse {
+    ApiError::EndpointNotFound {
+        hostname,
+        uri: req.uri().to_string(),
+    }
+}
