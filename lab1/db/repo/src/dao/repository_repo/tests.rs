ea330820c79e00a49f74fb54b059bf16f137b146
Antonio <98595473+tonujet@users.noreply.github.com>
2024-02-29 15:46:08 +0200

    Add repository layer for repo entity

---
+++ lab1/db/repo/src/dao/repository_repo/tests.rs
@@ -0,0 +1,51 @@
+use sea_orm::DatabaseBackend;
+use serial_test::serial;
+
+use crate::utils::repository::repository_test_helper;
+
+use super::RepositoryRepo;
+use super::RepositoryRepoTrait;
+
+pub async fn get_mock_repo() -> impl RepositoryRepoTrait {
+    repository_test_helper::get_test_model();
+    let alive_repo_dto = repository_test_helper::get_test_model();
+
+    let mut deleted_repo_dto = alive_repo_dto.clone();
+    deleted_repo_dto.deleted = true;
+
+    let conn = sea_orm::MockDatabase::new(DatabaseBackend::Postgres)
+        .append_query_results([
+            [alive_repo_dto.clone()],
+            [alive_repo_dto],
+            [deleted_repo_dto.clone()],
+            [deleted_repo_dto],
+        ])
+        .into_connection();
+
+    RepositoryRepo::new(conn)
+}
+
+#[tokio::test]
+#[serial]
+async fn it_should_not_update_deleted_repo() {
+    let repository_repo = get_mock_repo().await;
+    let create_repo = repository_test_helper::get_test_create_dto();
+
+    let repo = repository_repo.create(create_repo).await.unwrap();
+    let repo = repository_repo.delete(repo.id).await.unwrap();
+    let update_repo = repository_test_helper::get_test_update_dto();
+
+    let repo = repository_repo.update(repo.id, update_repo).await;
+    assert!(repo.is_err());
+}
+
+#[tokio::test]
+#[serial]
+async fn it_should_not_delete_deleted_repo() {
+    let repository_repo = get_mock_repo().await;
+    let create_repo = repository_test_helper::get_test_create_dto();
+    let repo = repository_repo.create(create_repo).await.unwrap();
+    let repo = repository_repo.delete(repo.id).await.unwrap();
+    let repo = repository_repo.delete(repo.id).await;
+    assert!(repo.is_err());
+}
