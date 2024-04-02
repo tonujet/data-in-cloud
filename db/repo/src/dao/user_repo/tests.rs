6d16b7dfaa0e616bdd5f2155c884801fb640af06
Antonio <98595473+tonujet@users.noreply.github.com>
2024-04-02 23:03:31 +0300

    Add test to user repository layer

---
+++ db/repo/src/dao/user_repo/tests.rs
@@ -0,0 +1,131 @@
+use mongodb::bson::doc;
+use collection::user::User;
+use crate::dao::error::RepoResult;
+
+use crate::dao::RepoTrait;
+use crate::dao::user_repo::UserRepo;
+use crate::dto::user_dto::UserDto;
+use crate::utils::user::user_test_helper;
+
+#[tokio::test]
+async fn list_all_not_deleted_users_success() {
+    let repo = user_test_helper::get_mock_repo();
+    let create_dtos = user_test_helper::get_create_dtos();
+    let mut ids = Vec::new();
+    let mut counter = create_dtos.len();
+    let take = 0;
+    let skip = 0;
+
+    for dto in create_dtos {
+        let created_dto = repo.create(dto).await;
+        assert!(created_dto.is_ok());
+        ids.push(created_dto.unwrap().id.unwrap())
+    }
+
+    for (i, id) in ids.into_iter().enumerate() {
+        if i % 2 == 0 {
+            let deleted = repo.delete(id).await;
+            assert!(deleted.is_ok());
+            counter -= 1;
+        }
+    }
+
+    let created_dtos = repo.list(Some(take), Some(skip)).await;
+    assert!(created_dtos.is_ok());
+    let created_dtos = created_dtos.unwrap();
+    assert_eq!(created_dtos.count, counter as u64)
+}
+
+#[tokio::test]
+async fn get_deleted_user_failure() {
+    let repo = user_test_helper::get_mock_repo();
+    let created_dto = run_create_dto1(&repo).await;
+
+    let deleted_dto = repo.delete(created_dto.id.unwrap()).await;
+    assert!(deleted_dto.is_ok());
+
+    let doc = repo
+        .get_user(doc! {"_id": deleted_dto.unwrap().id.unwrap()})
+        .await;
+    assert!(doc.is_err())
+}
+
+#[tokio::test]
+async fn get_user_success() {
+    let repo = user_test_helper::get_mock_repo();
+    let created_dto = run_create_dto1(&repo).await;
+    
+    let doc = repo
+        .get_user(doc! {"_id": created_dto.id.unwrap()})
+        .await;
+    assert!(doc.is_ok())
+}
+
+#[tokio::test]
+async fn analyze_responses_to_uniqueness_success() {
+    let repo = user_test_helper::get_mock_repo();
+    let created_dto = run_create_dto1(&repo).await;
+    let key_field = "email";
+
+    let email_res = repo
+        .get_user(doc! {key_field: created_dto.email})
+        .await;
+
+    let reses = vec![(email_res, key_field)];
+    let analized = repo.analyze_reses_to_uniqueness(reses);
+    assert!(analized.is_err())
+}
+
+#[tokio::test]
+async fn analyze_responses_to_uniqueness_failure() {
+    let repo = user_test_helper::get_mock_repo();
+    let reses: Vec<(RepoResult<User>, &'static str)> = vec![];
+    
+    let analized = repo.analyze_reses_to_uniqueness(reses);
+    
+    assert!(analized.is_ok())
+}
+
+
+#[tokio::test]
+async fn validate_update_uniqueness_success(){
+    let repo = user_test_helper::get_mock_repo();
+    let update_dto = user_test_helper::get_update_dto();
+    let user = user_test_helper::get_created1();
+    
+    let validated = repo.validate_update_uniqueness(&user, &update_dto).await;
+    
+    assert!(validated.is_ok())
+}
+
+
+
+#[tokio::test]
+async fn validate_update_uniqueness_failure(){
+    let repo = user_test_helper::get_mock_repo();
+    let _ = run_create_dto1(&repo).await;
+    let created_dto2 = run_create_dto2(&repo).await;
+    
+    let mut update_dto = user_test_helper::get_update_dto();
+    update_dto.username = created_dto2.username.clone();
+    let user = user_test_helper::get_created1();
+    
+    let validated = repo.validate_update_uniqueness(&user, &update_dto).await;
+
+    assert!(validated.is_err())
+}
+
+
+async fn run_create_dto1(repo: &UserRepo) -> UserDto {
+    let create_dto = user_test_helper::get_create_dto1();
+    let created_dto = repo.create(create_dto).await;
+    assert!(created_dto.is_ok());
+    return created_dto.unwrap()
+}
+
+async fn run_create_dto2(repo: &UserRepo) -> UserDto {
+    let create_dto = user_test_helper::get_create_dto2();
+    let created_dto = repo.create(create_dto).await;
+    assert!(created_dto.is_ok());
+    return created_dto.unwrap()
+}
---
+++ db/repo/src/utils/user/user_test_helper.rs
@@ -0,0 +1,127 @@
+use std::sync::Arc;
+use mongodb::bson::oid::ObjectId;
+
+use collection::user::{TestUserCollection, User};
+use collection::MongoCollection;
+
+use crate::dao::user_repo::UserRepo;
+use crate::dto::user_dto::{CreateUserDto, UpdateUserDto, UserDto};
+
+pub fn get_mock_repo() -> UserRepo {
+    let collection: Arc<dyn MongoCollection<User>> = Arc::new(TestUserCollection::new());
+    UserRepo { collection }
+}
+
+pub fn get_create_dto1() -> CreateUserDto {
+    CreateUserDto {
+        email: "create@gmail.com".to_string(),
+        username: "create".to_string(),
+        password: "create".to_string(),
+        age: 55,
+        is_public: true,
+    }
+}
+
+pub fn get_create_dto2() -> CreateUserDto {
+    CreateUserDto {
+        email: "create2@gmail.com".to_string(),
+        username: "create2".to_string(),
+        password: "create2".to_string(),
+        age: 11,
+        is_public: false,
+    }
+}
+
+
+pub fn get_created1() -> User {
+    User {
+        id: Some(ObjectId::new()),
+        email: "create@gmail.com".to_string(),
+        username: "create".to_string(),
+        password: "create".to_string(),
+        age: 55,
+        is_public: true,
+        deleted: false,
+        created: Default::default(),
+        updated: Default::default(),
+    }
+}
+
+
+pub fn get_created2() -> User {
+    User {
+        id: Some(ObjectId::new()),
+        email: "create2@gmail.com".to_string(),
+        username: "create2".to_string(),
+        password: "create2".to_string(),
+        age: 11,
+        is_public: false,
+        deleted: false,
+        created: Default::default(),
+        updated: Default::default(),
+    }
+}
+
+pub fn get_created_dto1() -> UserDto {
+    get_created1().into()
+}
+
+pub fn get_created_dto2() -> UserDto {
+    get_created2().into()
+}
+
+pub fn get_create_dtos() -> Vec<CreateUserDto> {
+    vec![
+        CreateUserDto {
+            email: "email1@gmail.com".to_string(),
+            username: "username1".to_string(),
+            password: "password1".to_string(),
+            age: 11,
+            is_public: true,
+        },
+        CreateUserDto {
+            email: "email2@gmail.com".to_string(),
+            username: "username2".to_string(),
+            password: "password2".to_string(),
+            age: 13,
+            is_public: false,
+        },
+        CreateUserDto {
+            email: "email3@gmail.com".to_string(),
+            username: "username3".to_string(),
+            password: "password3".to_string(),
+            age: 17,
+            is_public: true,
+        },
+        CreateUserDto {
+            email: "email4@gmail.com".to_string(),
+            username: "username4".to_string(),
+            password: "password4".to_string(),
+            age: 33,
+            is_public: true,
+        },
+    ]
+}
+
+pub fn get_update_dto() -> UpdateUserDto {
+   UpdateUserDto {
+       username: "update".to_string(),
+       age: 11,
+       is_public: true,
+   } 
+}
+
+pub fn get_updated() -> User {
+    let update_dto = get_update_dto();
+    let mut created = get_created1();
+    created.username = update_dto.username;
+    created.age = update_dto.age; 
+    created.is_public = update_dto.is_public;
+    created
+}
+
+
+pub fn get_updated_dto() -> UserDto {
+    get_updated().into()
+}
+
