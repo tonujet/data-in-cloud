21dcf0a92ec0c2e6e8c819d5f644674539934baa
Antonio <98595473+tonujet@users.noreply.github.com>
2024-04-29 00:24:15 +0300

    Update naming

--- src/web/service/mod.rs
+++ src/web/service/mod.rs
@@ -3,19 +3,15 @@ use mongodb::bson::oid::ObjectId;
 use uuid::Uuid;
 
 use repo::dto::user_dto::{CreateUserDto, UpdateUserDto, UserDto};
-use repo::dto::user_repo_info_dto::{CreateUserRepoInfoDto, UserRepoInfoDto};
 use repo::dto::{
-    repo_dto::{CreateUpdateRepoDto, RepoDto},
+    repository_dto::{CreateUpdateRepoDto, RepoDto},
     DtoList,
 };
-
 use crate::web::dto::user_repo_dto::{UserMultipleRepo, UserSingleRepo};
 
 use super::error::ApiResult;
 
 pub mod repo_service;
-pub mod user_repo_info_receiver;
-pub mod user_repo_info_service;
 pub mod user_repo_service;
 pub mod user_service;
 
@@ -28,31 +24,10 @@ pub trait ServiceTrait<C, U, R, I>: Send + Sync {
     async fn list(&self, take: Option<u64>, offset: Option<u64>) -> ApiResult<DtoList<R>>;
 }
 
-#[async_trait]
-pub trait UserServiceTrait: ServiceTrait<CreateUserDto, UpdateUserDto, UserDto, ObjectId> {
-    async fn list_user_repos_info(
-        &self,
-        id: ObjectId,
-        page: Option<u64>,
-        offset: Option<u64>,
-    ) -> ApiResult<DtoList<UserRepoInfoDto>>;
-}
+pub trait UserServiceTrait: ServiceTrait<CreateUserDto, UpdateUserDto, UserDto, ObjectId> {}
 
 pub trait RepoServiceTrait:
     ServiceTrait<CreateUpdateRepoDto, CreateUpdateRepoDto, RepoDto, Uuid>
-{
-}
-
-#[async_trait]
-pub trait PersistentServiceTrait<C, R, I>: Send + Sync {
-    async fn create(&self, dto: C) -> ApiResult<R>;
-    async fn get(&self, id: &I) -> ApiResult<R>;
-    async fn list(&self, take: Option<u64>, offset: Option<u64>) -> ApiResult<DtoList<R>>;
-}
-
-#[async_trait]
-pub trait UserRepoInfoServiceTrait:
-    PersistentServiceTrait<CreateUserRepoInfoDto, UserRepoInfoDto, ObjectId>
 {
 }
 
@@ -63,7 +38,4 @@ pub trait BlobConnServiceTrait<K, V, S, M>: Send + Sync {
     async fn delete_pair(&self, key_id: &K, val_id: &V) -> ApiResult<S>;
 }
 
-pub trait UserRepoServiceTrait:
-    BlobConnServiceTrait<ObjectId, Uuid, UserSingleRepo, UserMultipleRepo>
-{
-}
+pub trait UserRepoServiceTrait: BlobConnServiceTrait<ObjectId, Uuid, UserSingleRepo, UserMultipleRepo> {}
