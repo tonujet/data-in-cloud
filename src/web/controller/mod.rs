020fad229ce04e147ba33bbe8ebf67e84ef90cbf
Antonio <98595473+tonujet@users.noreply.github.com>
2024-04-29 00:24:15 +0300

    Update naming

--- src/web/controller/mod.rs
+++ src/web/controller/mod.rs
@@ -4,14 +4,14 @@ use serde::Deserialize;
 use super::error::ApiResult;
 use super::state::*;
 
-mod repository_controller;
+mod repo_controller;
 mod user_controller;
 mod user_repo_controller;
 
 pub fn api_routes(state: AppState) -> Router {
     let router: Router<()> = Router::new();
     let api_router = Router::new()
-        .nest("/repos", repository_controller::routes(state.clone()))
+        .nest("/repos", repo_controller::routes(state.clone()))
         .nest("/users", user_controller::routes(state.clone()))
         .nest("/users", user_repo_controller::routes(state.clone()));
     router.nest("/api/v1", api_router)
