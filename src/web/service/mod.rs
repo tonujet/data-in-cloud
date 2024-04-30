e75a7bad0001d0f33525ebd7ee0fd61d004deda7
Antonio <98595473+tonujet@users.noreply.github.com>
2024-04-30 23:23:14 +0300

    Finish mvc model for user_repo_info entity

--- src/web/service/mod.rs
+++ src/web/service/mod.rs
@@ -2,16 +2,20 @@ use async_trait::async_trait;
 use mongodb::bson::oid::ObjectId;
 use uuid::Uuid;
 
-use repo::dto::user_dto::{CreateUserDto, UpdateUserDto, UserDto};
 use repo::dto::{
-    repository_dto::{CreateUpdateRepoDto, RepoDto},
     DtoList,
+    repo_dto::{CreateUpdateRepoDto, RepoDto},
 };
+use repo::dto::user_dto::{CreateUserDto, UpdateUserDto, UserDto};
+use repo::dto::user_repo_info_dto::{CreateUserRepoInfoDto, UserRepoInfoDto};
+
 use crate::web::dto::user_repo_dto::{UserMultipleRepo, UserSingleRepo};
 
 use super::error::ApiResult;
 
 pub mod repo_service;
+pub mod user_repo_info_receiver;
+pub mod user_repo_info_service;
 pub mod user_repo_service;
 pub mod user_service;
 
@@ -32,10 +36,32 @@ pub trait RepoServiceTrait:
 }
 
 #[async_trait]
+pub trait PersistentServiceTrait<C, R, I>: Send + Sync {
+    async fn create(&self, dto: C) -> ApiResult<R>;
+    async fn get(&self, id: &I) -> ApiResult<R>;
+    async fn list(&self, take: Option<u64>, offset: Option<u64>) -> ApiResult<DtoList<R>>;
+}
+
+pub trait UserRepoInfoServiceTrait:
+    PersistentServiceTrait<CreateUserRepoInfoDto, UserRepoInfoDto, ObjectId>
+{
+}
+
+#[async_trait]
 pub trait BlobConnServiceTrait<K, V, S, M>: Send + Sync {
     async fn add_pair(&self, key_id: &K, val_id: &V) -> ApiResult<S>;
     async fn list_pairs(&self, key_id: &K, take: Option<u64>, offset: Option<u64>) -> ApiResult<M>;
     async fn delete_pair(&self, key_id: &K, val_id: &V) -> ApiResult<S>;
 }
 
-pub trait UserRepoServiceTrait: BlobConnServiceTrait<ObjectId, Uuid, UserSingleRepo, UserMultipleRepo> {}
+pub trait UserRepoServiceTrait:
+    BlobConnServiceTrait<ObjectId, Uuid, UserSingleRepo, UserMultipleRepo>
+{
+}
+
+#[async_trait]
+pub trait ReceiverTrait<M: Send + Sync>: Send + Sync {
+    async fn receive(&self) -> ApiResult<M>;
+}
+
+pub trait UserRepoInfoReceiverTrait: ReceiverTrait<UserRepoInfoDto>{}
