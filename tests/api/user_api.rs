67aee81f4684d92a587ce205f67313dbf4ac6af0
Antonio <98595473+tonujet@users.noreply.github.com>
2024-05-03 16:07:43 +0300

    Move boilerplate code to helpers in the integration tests

--- tests/api/user_api.rs
+++ tests/api/user_api.rs
@@ -8,11 +8,13 @@ use repo::utils::user::user_test_helper;
 
 use crate::common::Setup;
 
+use crate::helpers::user_api_helper;
+
 #[tokio::test]
 #[serial]
 async fn create_user_success() {
     let setup = Setup::new().await;
-    create_user1(&setup.client).await;
+    user_api_helper::create_user1(&setup.client).await;
 }
 
 #[tokio::test]
@@ -22,7 +24,7 @@ async fn create_user_with_taken_email_and_username_failure() {
     let create_dto = user_test_helper::get_create_dto1();
     let expected_code = StatusCode::CONFLICT;
 
-    create_user1(&setup.client).await;
+    user_api_helper::create_user1(&setup.client).await;
     let res = setup.client.post("/api/v1/users").json(&create_dto).await;
 
     assert_eq!(res.status_code(), expected_code);
@@ -36,7 +38,7 @@ async fn update_user_success() {
     let expected_code = StatusCode::OK;
     let expected_user_dto = user_test_helper::get_updated_dto();
 
-    let user_dto = create_user1(&setup.client).await;
+    let user_dto = user_api_helper::create_user1(&setup.client).await;
     let res = setup
         .client
         .put(&format!("/api/v1/users/{}", user_dto.id.unwrap()))
@@ -54,8 +56,8 @@ async fn update_user_with_taken_username_failure() {
     let setup = Setup::new().await;
     let expected_code = StatusCode::CONFLICT;
 
-    let user1_dto = create_user1(&setup.client).await;
-    let user2_dto = create_user2(&setup.client).await;
+    let user1_dto = user_api_helper::create_user1(&setup.client).await;
+    let user2_dto = user_api_helper::create_user2(&setup.client).await;
     let mut update_dto = user_test_helper::get_update_dto();
     update_dto.username = user1_dto.username;
     let res = setup
@@ -73,7 +75,7 @@ async fn delete_user_success() {
     let setup = Setup::new().await;
     let expected_delete_code = StatusCode::OK;
     let expected_get_code = StatusCode::CONFLICT;
-    let user_dto = create_user1(&setup.client).await;
+    let user_dto = user_api_helper::create_user1(&setup.client).await;
 
     let res = setup
         .client
@@ -110,7 +112,7 @@ async fn delete_nonexistent_user_failure() {
 #[serial]
 async fn get_user_success() {
     let setup = Setup::new().await;
-    let user_dto = create_user1(&setup.client).await;
+    let user_dto = user_api_helper::create_user1(&setup.client).await;
     let expected_code = StatusCode::OK;
 
     let res = setup
@@ -143,7 +145,7 @@ async fn get_notexistent_user_failure() {
 async fn list_all_users_success() {
     let setup = Setup::new().await;
     let expected_code = StatusCode::OK;
-    let created_dtos = create_users(&setup.client).await;
+    let created_dtos = user_api_helper::create_users(&setup.client).await;
     let expected_count = created_dtos.len() as u64;
 
     
@@ -159,7 +161,7 @@ async fn list_all_users_success() {
 async fn list_users_using_take_and_skip_success() {
     let setup = Setup::new().await;
     let expected_code = StatusCode::OK;
-    let created_dtos = create_users(&setup.client).await;
+    let created_dtos = user_api_helper::create_users(&setup.client).await;
     let expected_count = created_dtos.len() as u64;
     let expected_len = created_dtos[1..4].len();
 
@@ -174,47 +176,4 @@ async fn list_users_using_take_and_skip_success() {
     assert_eq!(res.status_code(), expected_code);
     assert_eq!(dtos.dtos.len(), expected_len);
     assert_eq!(dtos.count, expected_count);
-}
-
-async fn create_user1(client: &TestServer) -> UserDto {
-    let create_dto = user_test_helper::get_create_dto1();
-    let expected_status_code = StatusCode::OK;
-    let expected_user_dto = user_test_helper::get_created_dto1();
-
-    let res = client.post("/api/v1/users").json(&create_dto).await;
-    let user_dto: UserDto = res.json();
-
-    assert_eq!(res.status_code(), expected_status_code);
-    assert_eq!(user_dto, expected_user_dto);
-
-    user_dto
-}
-
-async fn create_user2(client: &TestServer) -> UserDto {
-    let create_dto = user_test_helper::get_create_dto2();
-    let expected_status_code = StatusCode::OK;
-    let expected_created_dto = user_test_helper::get_created_dto2();
-
-    let res = client.post("/api/v1/users").json(&create_dto).await;
-    let created_dto: UserDto = res.json();
-
-    assert_eq!(res.status_code(), expected_status_code);
-    assert_eq!(created_dto, expected_created_dto);
-    created_dto
-}
-
-async fn create_users(client: &TestServer) -> Vec<UserDto> {
-    let create_dtos = user_test_helper::get_create_dtos();
-    let expected_status_code = StatusCode::OK;
-    let mut created_dtos = Vec::new();
-
-    for dto in create_dtos {
-        let res = client.post("/api/v1/users").json(&dto).await;
-        assert_eq!(res.status_code(), expected_status_code);
-
-        let user_dto: UserDto = res.json();
-        created_dtos.push(user_dto);
-    }
-
-    created_dtos
 }
